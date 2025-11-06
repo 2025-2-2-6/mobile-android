cd62feeed5a2c823ffe4871cd8622e605ac89f4d
jangjaewoong <jao6364@naver.com>
2025-11-06 19:10:44 +0900

    feat: URL 입력 UI 추가

---
+++ app/src/main/java/com/example/mobile_android/AddSiteActivity.java
@@ -0,0 +1,12 @@
+package com.example.mobile_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSiteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_site);
+    }
+}
